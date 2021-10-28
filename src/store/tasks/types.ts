19a3a04b0385371fc3af423db2a1d4091ed7db78
Evheny <strkromik@gmail.com>
2021-10-28 14:13:08 +0300

    add basic redux functionality

--- src/store/tasks/types.ts
+++ src/store/tasks/types.ts
@@ -1,3 +1,18 @@
-// import { Action } from 'redux';
+import { ITask } from '../../shared/interfaces';
 
-export type TTasksActions = {};
+export const ADD_TASK = 'ADD_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const EDIT_TASK = 'EDIT_TASK';
+
+// export type TTaskActions = ADD_TASK | DELETE_TASK | EDIT_TASK
+
+export type TaskState = {
+  tasks: ITask[];
+};
+
+export type TaskAction = {
+  type: string;
+  task: ITask;
+};
+
+export type DispatchType = (args: TaskAction) => TaskAction;
